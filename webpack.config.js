8ce6e79a5b3e4048b6b42cced7754f1106fdc992
Alexey Baranov <65853601+Baranov-alexei18@users.noreply.github.com>
2025-07-29 16:38:03 +0300

    check3

--- webpack.config.js
+++ webpack.config.js
@@ -39,12 +39,10 @@ module.exports = {
         react: {
           singleton: true,
           requiredVersion: '^18.2.0',
-          import: false
         },
         'react-dom': {
           singleton: true,
           requiredVersion: '^18.2.0',
-          import: false
         },
       },
     }),
