b010191a6553e66b2e76a3acc77987b0a803c31c
Alexey Baranov <65853601+Baranov-alexei18@users.noreply.github.com>
2025-07-29 15:44:55 +0300

    wmf vercel

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].js',
-    publicPath: 'https://host-app-xois.vercel.app/',
+    publicPath: 'auto',
     clean: true,
   },
   resolve: {
